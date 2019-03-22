464aaaf972ab8375d60d742dd132b3ff1beec462
luffyrene <reneau1998@gmail.com>
2019-03-23 02:07:53 +0800

    edit login.js

---
+++ client/src/pages/login/login.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    validateForm() {
+        return this.state.username.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="email" bsSize="large">
+                        <Form.Label>Username</Form.Label>
+                        <FormControl
+                            autoFocus
+                            type="username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bsSize="large">
+                        <Form.Label>Password</Form.Label>
+                        <FormControl
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        bsSize="large"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
