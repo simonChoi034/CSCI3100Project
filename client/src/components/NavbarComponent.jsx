c74adbe0dc5e03afe5a6a780cb814ef998e92b3a
luffyrene <reneau1998@gmail.com>
2019-03-22 21:25:25 +0800

    edit navigation bar

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -55,7 +55,7 @@ class NavbarComponent extends Component {
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
-                    Option 1
+                    Cases Wall
                   </DropdownItem>
                   <DropdownItem>
                     Option 2
