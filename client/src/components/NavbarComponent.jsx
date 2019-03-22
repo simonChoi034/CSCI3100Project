749223f7b7b6f3b7a65b2e2e177a2d1056e3c270
luffyrene <reneau1998@gmail.com>
2019-03-22 21:44:20 +0800

    edit navigation bar

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -1,77 +1,83 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './NavbarComponent.css';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Input } from 'reactstrap';
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    Input
+} from 'reactstrap';
 
 
 class NavbarComponent extends Component {
 
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div className="NavbarComponent">
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
 
-        <Navbar className="bg-dark" expand="md">
-          <NavbarBrand className="btn btn-warning text-light" href="/">TeachHub</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-            <InputGroup>
-              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
-              <Input placeholder="Search..." />
-              <button>Search</button>
-            </InputGroup>
-              <NavItem>
-                <NavLink className="text-light" href="/tutors/">Tutors</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle className="text-light" nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Cases Wall
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Option 3
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div className="NavbarComponent">
+
+                <Navbar className="bg-dark" expand="md">
+                    <NavbarBrand className="btn btn-warning text-light" href="/">TeachHub</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <InputGroup>
+                                <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+                                <Input placeholder="Search..."/>
+                                <button>Search</button>
+                            </InputGroup>
+                            <NavItem>
+                                <NavLink className="text-light" href="/tutors/">Tutors</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle className="text-light" nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Cases Wall
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Tutors List
+                                    </DropdownItem>
+                                    <DropdownItem divider/>
+                                    <DropdownItem>
+                                        Register
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Log in
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default NavbarComponent;
