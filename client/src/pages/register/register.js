9a7f111e713afad5d0cc90a33ffbbe7105947fc0
luffyrene <reneau1998@gmail.com>
2019-03-23 12:36:31 +0800

    modified register.js register.css tutors.js tutors.css jobs_wall.js jobs_wall.css

---
+++ client/src/pages/register/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./register.css";
+
+const Register = () => {
+    return (
+        <div>
+            <p>Register Page</p>
+        </div>
+    );
+}
+
+
+export default Register;
---
+++ client/src/pages/tutors/tutors.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./tutors.css";
+
+const Tutors = () => {
+    return (
+        <div>
+            <p>Tutors List Page</p>
+        </div>
+    );
+}
+
+export default Tutors;
