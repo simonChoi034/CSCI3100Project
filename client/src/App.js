3af02c1e559efb93eb9cf7f97dcb3de754f610f2
SteinsFu <fuwingwa@hotmail.com>
2019-04-04 00:43:13 +0800

    Add react-particles-js & add ParticlesContainer.css & add ParticlesContainer.jsx in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-import NavbarComponent from './components/navbar/NavbarComponent'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NavbarComponent from './components/navbar/NavbarComponent';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ParticlesContainer from './components/particles_container/ParticlesContainer';
 import Home from './pages/home.js';
 import Login from './pages/login.js';
 import Register from './pages/register.js';
@@ -13,16 +14,21 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <NavbarComponent />
                 <Router>
-                    <Switch>
-                        <Route path='/' component={Home} exact />
-                        <Route path='/login' component={Login} />
-                        <Route path='/register' component={Register} />
-                        <Route path='/tutors' component={Tutors} />
-                        <Route path='/jobs' component={Jobs} />
-                        <Route path='/forget_pw' component={Forget_PW} />
-                    </Switch>
+                    <div className="particles-bg">
+                        <ParticlesContainer />
+                        <div className="web-content">
+                            <NavbarComponent />
+                            <Switch>
+                                <Route path='/' component={Home} exact />
+                                <Route path='/login' component={Login} />
+                                <Route path='/register' component={Register} />
+                                <Route path='/tutors' component={Tutors} />
+                                <Route path='/jobs' component={Jobs} />
+                                <Route path='/forget_pw' component={Forget_PW} />
+                            </Switch>
+                        </div>
+                    </div>
                 </Router>
             </div>
         );
