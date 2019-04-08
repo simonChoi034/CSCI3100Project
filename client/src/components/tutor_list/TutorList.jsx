7a6bc33ae0cac730cf22b17cf4b6e9318a9dbec3
luffyrene <reneau1998@gmail.com>
2019-04-08 23:57:52 +0800

    create tutor list page modified: user.js created: TutorList.jsx TUtorList.css TutorCard.jsx TutorCard.css

---
+++ client/src/components/tutor_list/TutorList.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './TutorList.css';
+import TutorCard from '../tutor_card/TutorCard';
+import {
+    Container,
+    Row,
+} from 'reactstrap';
+
+class TutorList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tutors: []  // temp data
+        };
+    }
+
+    componentDidMount() {
+        axios.get("/api/user/list_tutor")
+            .then(res => {
+                this.setState({tutors: res.data.tutorList})
+            })
+            .catch(err => console.error(err.toString()))
+    }
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    {this.state.tutors.map((tutor) =>
+                        <TutorCard tutor={tutor} />
+                    )}
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default TutorList;
