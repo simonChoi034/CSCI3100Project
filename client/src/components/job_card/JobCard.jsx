21d140c1f89fa9d84194bb7ad1adf6506ffbf223
SteinsFu <fuwingwa@hotmail.com>
2019-04-10 09:43:59 +0800

    deleted useless code

--- client/src/components/job_card/JobCard.jsx
+++ client/src/components/job_card/JobCard.jsx
@@ -27,21 +27,6 @@ class JobCard extends Component {
                     <CardText className="text-left">District: {this.state.job.district}</CardText>
                     <CardText className="text-left">Student Level: {this.state.job.student_level}</CardText>
                     <CardText className="text-left">Tuition Fee: {this.state.job.tuition_fee}</CardText>
-                    {
-                    /*
-                    <CardText className="text-left">Total Number of Student: {this.state.job.num_of_student}</CardText>
-                    <CardText className="text-left">Times per week: {this.state.job.times_per_week}</CardText>
-                    <CardText className="text-left">Duration: {this.state.job.duration}</CardText>
-                    <CardText className="text-left">Lesson Time: {this.state.job.lesson_time}</CardText>
-                    <CardText className="text-left">Remark: {this.state.job.remark}</CardText> 
-                    
-                    <Button 
-                        className = "more_btn"
-                        onClick={this.handleClick}>
-                        {this.out}
-                    </Button>
-                    */
-                    }
                     <Button color="info" className="detail_btn" onClick={ (event) => this.props.toggle(event, this.state.job)}>Show Detail</Button>
                 </Card>
             </Col>
