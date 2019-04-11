45b9a334bd650c7b9c7249c6b0f2a05675d86269
simonChoi034 <simonchoi034@gmail.com>
2019-04-12 00:08:21 +0800

    Bug fix in migration

--- migrations/20190411144843_create_messager.js
+++ migrations/20190411144843_create_messager.js
@@ -9,7 +9,7 @@ exports.up = function(knex, Promise) {
         }),
         knex.schema.createTable('conversation_reply', function (table) {
             table.increments('cr_id').primary();
-            table.text('messenger.js');
+            table.text('message');
             table.integer('user_id').notNullable().unsigned().references('user_account.id');
             table.dateTime('time').notNullable().defaultTo(knex.fn.now());
             table.integer('c_id').notNullable().unsigned().references('conversation.c_id');
