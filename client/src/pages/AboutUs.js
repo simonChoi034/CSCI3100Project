7f9482b05adfb6350697b77f8a30058b3cb84cd5
luffyrene <reneau1998@gmail.com>
2019-04-12 02:02:23 +0800

    add container and card to about us

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
 import './AboutUs.css';
+import { Container, Card } from 'reactstrap';
 
 class AboutUs extends Component {
     render() {
         return (
             <div>
-                <p>What is TeachHub?</p>
-                <p>We provide an interactive platform for students and tutors matching in an convenient way.
-                    Therefore, students can enhance their academic results or learn new non-academic skills while private tutors can earn a living.</p>
-                <p>Why you should use TeachHub?</p>
-                <p>Easy to use</p>
-                <p>We provide a user-friendly chatroom for students and tutors to communicate easily</p>
-                <p>What you can find in TeachHub?</p>
-                <p>Students can find the most suitable tutor</p>
-                <p>Tutors can find a tutoring job in the easiest way</p>
+                <Container className="my-3">
+                    <Card className="p-3">
+                        <h2>What is TeachHub?</h2>
+                        <p>We provide an interactive platform for students and tutors matching in an convenient way.
+                            Therefore, students can enhance their academic results or learn new non-academic skills while private tutors can earn a living.</p>
+                    </Card>
+                </Container>
+                <Container className="my-3">
+                    <Card className="p-3">
+                        <h2>Why you should use TeachHub?</h2>
+                        <p>Easy to use</p>
+                        <p>We provide a user-friendly chatroom for students and tutors to communicate easily</p>
+                    </Card>
+                </Container>
+                <Container className="my-3">
+                    <Card className="p-3">
+                        <h2>What you can find in TeachHub?</h2>
+                        <p>Students can find the most suitable tutor</p>
+                        <p>Tutors can find a tutoring job in the easiest way</p>
+                    </Card>
+                </Container>
             </div>
         );
     }
