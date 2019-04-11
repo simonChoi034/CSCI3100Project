62f7c0378f0f39fe6bc03c2d0fc49955551859d1
luffyrene <reneau1998@gmail.com>
2019-04-12 02:05:22 +0800

    fix grammatical mistakes

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -9,22 +9,22 @@ class AboutUs extends Component {
                 <Container className="my-3">
                     <Card className="p-3">
                         <h2>What is TeachHub?</h2>
-                        <p>We provide an interactive platform for students and tutors matching in an convenient way.
-                            Therefore, students can enhance their academic results or learn new non-academic skills while private tutors can earn a living.</p>
+                        <p>We provide an interactive platform for students and tutors matching in an convenient way.</p>
+                        <p>Therefore, students can enhance their academic results or learn new non-academic skills while private tutors can earn a living.</p>
                     </Card>
                 </Container>
                 <Container className="my-3">
                     <Card className="p-3">
                         <h2>Why you should use TeachHub?</h2>
-                        <p>Easy to use</p>
-                        <p>We provide a user-friendly chatroom for students and tutors to communicate easily</p>
+                        <p>Easy to use.</p>
+                        <p>We provide a user-friendly chatroom for students and tutors to communicate easily.</p>
                     </Card>
                 </Container>
                 <Container className="my-3">
                     <Card className="p-3">
                         <h2>What you can find in TeachHub?</h2>
-                        <p>Students can find the most suitable tutor</p>
-                        <p>Tutors can find a tutoring job in the easiest way</p>
+                        <p>Students can find the most suitable tutor.</p>
+                        <p>Tutors can find a tutoring job in the easiest way.</p>
                     </Card>
                 </Container>
             </div>
