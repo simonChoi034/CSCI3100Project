db42a731753a7e49ffd26317ff796f071205ea72
SteinsFu <fuwingwa@hotmail.com>
2019-04-12 04:10:10 +0800

    minor change

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -17,7 +17,7 @@ class Footer extends Component {
                         <a href = 'mailto:teachhub@gmail.com'>
                             <p id="contact_info"><IoMdMail className="mr-2 align-text-top"/> teachhub@gmail.com</p>
                         </a>
-                        <p id="contact_hotline"><FaPhone className="mr-2 align-text-top"/> 9388 9388</p>
+                        <p id="contact_hotline"><FaPhone className="mr-2 align-text-top"/> +852 9388 9388</p>
                     </Col>
                     <Col sm="0" md="3" xl="5"></Col>
                     <Col sm="12" md="5" xl="3" className="text-left pr-0 mt-4">
