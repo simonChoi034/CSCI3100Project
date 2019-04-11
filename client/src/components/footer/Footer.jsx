49d485cc2fa9873ff80f51a6c42762722c98e23e
SteinsFu <fuwingwa@hotmail.com>
2019-04-12 02:49:28 +0800

    adjust footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -10,15 +10,15 @@ class Footer extends Component {
             <div id="footer" className="mt-5 mb-0 px-5 py-3 text-light">
                 
                 <Row>
-                    <Col sm="12" md="6" className="text-left mt-4">
+                    <Col sm="12" md="4" className="text-left mt-4">
                         <h6 id="contact_us m-0">Contact Us:</h6>
                         <a href = 'mailto:teachhub@gmail.com'>
                             <p id="contact_info">Email: teachhub@gmail.com</p>
                         </a>
                         <p id="contact_hotline">Hotline: 9388 9388</p>
                     </Col>
-
-                    <Col sm="12" md="6" className="text-left mt-4">
+                    <Col sm="0" md="3" xl="5"></Col>
+                    <Col sm="12" md="5" xl="3" className="text-left pr-0 mt-4">
                         <p className="text-left">TeachHub © 2019 All Rights Reserved</p>
                     </Col>
                 </Row>
