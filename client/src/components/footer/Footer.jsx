d603e1f2afdff4fa92202f825044126b527eb34d
luffyrene <reneau1998@gmail.com>
2019-04-12 01:04:16 +0800

    add contact us to footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -9,6 +9,9 @@ class Footer extends Component {
         return (
             <div id="footer" className="mt-5 mb-0, px-0">
                 <p className="text-center m-0 text-light">TeachHub © 2019 All Rights Reserved</p>
+                <p id="contact_us">Contact Us</p>
+                <p id="contact_info">Email: teachhub@gmail.com</p>
+                <p id="contact_info">Hotline: 9388 9388</p>
             </div>
         )
     }
