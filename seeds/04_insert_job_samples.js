88959b80c4993ed506b5a0370232493c924ca1fb
luffyrene <reneau1998@gmail.com>
2019-04-12 17:12:31 +0800

    bugs not fix

--- seeds/04_insert_job_samples.js
+++ seeds/04_insert_job_samples.js
@@ -70,64 +70,52 @@ exports.seed = function(knex, Promise) {
           { user_id: 30, email: "tut14@gmail.com", phone: "90000030", full_name_ch: "陳十四", full_name_en: "ChanFourteen", nick_name: "Chan14", sex: "M", birth: "1998-2-14", education_level_id: "7", description: "I am experienced!"},
           { user_id: 31, email: "tut15@gmail.com", phone: "90000031", full_name_ch: "陳十五", full_name_en: "ChanFifteen", nick_name: "Chan15", sex: "F", birth: "1998-3-15", education_level_id: "1", description: "I am a good person!"},
           { user_id: 32, email: "tut16@gmail.com", phone: "90000032", full_name_ch: "陳十六", full_name_en: "ChanSixteen", nick_name: "Chan16", sex: "M", birth: "1998-4-16", education_level_id: "2", description: "I am a kind person!"}
-      ]),
-      knex('job').insert([
-           { client_id: 1, open: true, district_id: 1, location: 'Pacific Coffee', student_level_id: 1, tuition_fee: "$255/hr", num_of_student: 1, tutor_academic_id: 1, tutor_sex: 'F', times_per_week: '1 hrs', duration: '1 year', lesson_time: 'Monday 10:00am-11:00am', hotline: '9000 0001', remark: 'the tutor should be nice'},
-           { client_id: 2, open: true, district_id: 2, location: 'KFC', student_level_id: 2, tuition_fee: "$230/hr", num_of_student: 2, tutor_academic_id: 2, tutor_sex: 'M', times_per_week: '1.5 hrs', duration: '2 years', lesson_time: 'Tuesday 11:00am-12:30pm', hotline: '9000 0002', remark: 'want a patient tutor'},
-           { client_id: 3, open: true, district_id: 3, location: 'at home', student_level_id: 3, tuition_fee: "$299/hr", num_of_student: 3, tutor_academic_id: 3, tutor_sex: 'O', times_per_week: '2 hrs', duration: '3 years', lesson_time: 'Wednesday 9:00am-11:00am', hotline: '9000 0003', remark: 'need to be patient'},
-           { client_id: 4, open: true, district_id: 4, location: 'Library', student_level_id: 4, tuition_fee: "$211/hr", num_of_student: 4, tutor_academic_id: 4, tutor_sex: 'F', times_per_week: '2.5 hrs', duration: '4 years', lesson_time: 'Thursday 9:00am-11:30am', hotline: '9000 0004', remark: 'the tutor should be nice'},
-           { client_id: 5, open: true, district_id: 5, location: 'Study Room', student_level_id: 5, tuition_fee: "$260/hr", num_of_student: 5, tutor_academic_id: 5, tutor_sex: 'M', times_per_week: '3 hrs', duration: '5 years', lesson_time: 'Friday 10:00am-1:00pm', hotline: '9000 0005', remark: 'the tutor should be kind'},
-           { client_id: 6, open: true, district_id: 6, location: 'at home', student_level_id: 6, tuition_fee: "$300/hr", num_of_student: 1, tutor_academic_id: 6, tutor_sex: 'O', times_per_week: '3.5 hrs', duration: '6 years', lesson_time: 'Saturday 10:00am-1:30pm', hotline: '9000 0006', remark: 'the tutor should be a nice person'},
-           { client_id: 7, open: true, district_id: 7, location: 'McCafe', student_level_id: 7, tuition_fee: "$400/hr", num_of_student: 2, tutor_academic_id: 7, tutor_sex: 'F', times_per_week: '4 hrs', duration: '1.5 years', lesson_time: 'Sunday 10:00am-2:00pm', hotline: '9000 0007', remark: 'the tutor should be a kind person'},
-           { client_id: 8, open: true, district_id: 8, location: 'SUBWAY', student_level_id: 8, tuition_fee: "$200/hr", num_of_student: 3, tutor_academic_id: 1, tutor_sex: 'M', times_per_week: '4.5 hrs', duration: '2.5 years', lesson_time: 'Sunday 10:00am-2:30pm', hotline: '9000 0008', remark: 'the tutor should be patient'},
-           { client_id: 9, open: true, district_id: 9, location: 'McCafe', student_level_id: 9, tuition_fee: "$265/hr", num_of_student: 4, tutor_academic_id: 2, tutor_sex: 'O', times_per_week: '5 hrs', duration: '3.5 years', lesson_time: 'Saturday, Sunday 10:00am-12:30pm', hotline: '9000 0009', remark: 'the tutor should be a patient guy'},
-           { client_id: 10, open: true, district_id: 10, location: 'SUBWAY', student_level_id: 10, tuition_fee: "$275/hr", num_of_student: 5, tutor_academic_id: 3, tutor_sex: 'F', times_per_week: '5.5 hrs', duration: '4.5 years', lesson_time: 'Monday, Tuesday 10:00am-12:45pm', hotline: '9000 0010', remark: 'the tutor should be nice'},
-           { client_id: 11, open: true, district_id: 11, location: 'Pacific Coffee', student_level_id: 11, tuition_fee: "$285/hr", num_of_student: 1, tutor_academic_id: 4, tutor_sex: 'M', times_per_week: '6 hrs', duration: '5.5 years', lesson_time: 'Tuesday, Wednesday 10:00am-1:00pm', hotline: '9000 0011', remark: 'the tutor should be a nice guy'},
-           { client_id: 12, open: true, district_id: 12, location: 'KFC', student_level_id: 12, tuition_fee: "$295/hr", num_of_student: 2, tutor_academic_id: 5, tutor_sex: 'O', times_per_week: '6.5 hrs', duration: 'until DSE', lesson_time: 'Wednesday, Thursday 10:00am-1:15pm', hotline: '9000 0012', remark: 'the tutor should be kind'},
-           { client_id: 13, open: true, district_id: 13, location: 'Study Room', student_level_id: 1, tuition_fee: "$215/hr", num_of_student: 3, tutor_academic_id: 6, tutor_sex: 'F', times_per_week: '7 hrs', duration: '1 year', lesson_time: 'Thursday, Friday 10:00am-1:30pm', hotline: '9000 0013', remark: 'the tutor should be patient'},
-           { client_id: 14, open: true, district_id: 14, location: 'Library', student_level_id: 2, tuition_fee: "$225/hr", num_of_student: 4, tutor_academic_id: 7, tutor_sex: 'M', times_per_week: '7.5 hrs', duration: 'until DSE', lesson_time: 'Friday, Saturday 10:00am-1:45pm', hotline: '9000 0014', remark: 'the tutor should be kind'},
-           { client_id: 15, open: true, district_id: 15, location: 'McDonald\'s', student_level_id: 3, tuition_fee: "$235/hr", num_of_student: 5, tutor_academic_id: 1, tutor_sex: 'O', times_per_week: '8 hrs', duration: '1 year', lesson_time: 'Monday, Wednesday, Friday, Saturday 10:30am-12:30pm', hotline: '9000 0015', remark: 'want a nice tutor'},
-           { client_id: 16, open: true, district_id: 16, location: 'Starbucks', student_level_id: 4, tuition_fee: "$245/hr", num_of_student: 1, tutor_academic_id: 2, tutor_sex: 'F', times_per_week: '6 hrs', duration: 'until graduation', lesson_time: 'Tuesday, Thursday, Saturday 9:00am-11:00am', hotline: '9000 0016', remark: 'want a kind tutor'},
-           { client_id: 1, open: true, district_id: 17, location: 'McDonald\'s', student_level_id: 5, tuition_fee: "$199/hr", num_of_student: 2, tutor_academic_id: 3, tutor_sex: 'M', times_per_week: '6 hrs', duration: '1 year', lesson_time: 'Wednesday, Friday, Sunday 1:00pm-3:00pm', hotline: '9000 0017', remark: 'want a nice tutor'},
-           { client_id: 2, open: true, district_id: 18, location: 'Starbucks', student_level_id: 6, tuition_fee: "$399/hr", num_of_student: 3, tutor_academic_id: 4, tutor_sex: 'O', times_per_week: '6 hrs', duration: 'until graduation', lesson_time: 'Friday, Saturday, Sunday 3:00pm-5:00pm', hotline: '9000 0018', remark: 'want a patient tutor'}
-       ]),
-      knex('job_subject').insert([
-           { subject_id: 1},
-           { subject_id: 2},
-           { subject_id: 3},
-           { subject_id: 4},
-           { subject_id: 5},
-           { subject_id: 6},
-           { subject_id: 7},
-           { subject_id: 8},
-           { subject_id: 9},
-           { subject_id: 10},
-           { subject_id: 11},
-           { subject_id: 12},
-           { subject_id: 13},
-           { subject_id: 14},
-           { subject_id: 15},
-           { subject_id: 16},
-           { subject_id: 17},
-           { subject_id: 18}
-      //    { job_id: 1, subject_id: 1},
-      //    { job_id: 2, subject_id: 2},
-      //    { job_id: 3, subject_id: 3},
-      //    { job_id: 4, subject_id: 4},
-      //    { job_id: 5, subject_id: 5},
-      //    { job_id: 6, subject_id: 6},
-      //    { job_id: 7, subject_id: 7},
-      //    { job_id: 8, subject_id: 8},
-      //    { job_id: 9, subject_id: 9},
-      //    { job_id: 10, subject_id: 10},
-      //    { job_id: 11, subject_id: 11},
-      //    { job_id: 12, subject_id: 12},
-      //    { job_id: 13, subject_id: 13},
-      //    { job_id: 14, subject_id: 14},
-      //    { job_id: 15, subject_id: 15},
-      //    { job_id: 16, subject_id: 16},
-      //    { job_id: 17, subject_id: 17},
-      //    { job_id: 18, subject_id: 18}
       ])
+          .returning("*")
+          .then(function(result) {
+              return knex('job').insert([
+                  { client_id: result[0], open: true, district_id: 1, location: 'Pacific Coffee', student_level_id: 1, tuition_fee: "$255/hr", num_of_student: 1, tutor_academic_id: 1, tutor_sex: 'F', times_per_week: '1 hrs', duration: '1 year', lesson_time: 'Monday 10:00am-11:00am', hotline: '9000 0001', remark: 'the tutor should be nice'},
+                  { client_id: result[1], open: true, district_id: 2, location: 'KFC', student_level_id: 2, tuition_fee: "$230/hr", num_of_student: 2, tutor_academic_id: 2, tutor_sex: 'M', times_per_week: '1.5 hrs', duration: '2 years', lesson_time: 'Tuesday 11:00am-12:30pm', hotline: '9000 0002', remark: 'want a patient tutor'},
+                  { client_id: result[2], open: true, district_id: 3, location: 'at home', student_level_id: 3, tuition_fee: "$299/hr", num_of_student: 3, tutor_academic_id: 3, tutor_sex: 'O', times_per_week: '2 hrs', duration: '3 years', lesson_time: 'Wednesday 9:00am-11:00am', hotline: '9000 0003', remark: 'need to be patient'},
+                  { client_id: result[3], open: true, district_id: 4, location: 'Library', student_level_id: 4, tuition_fee: "$211/hr", num_of_student: 4, tutor_academic_id: 4, tutor_sex: 'F', times_per_week: '2.5 hrs', duration: '4 years', lesson_time: 'Thursday 9:00am-11:30am', hotline: '9000 0004', remark: 'the tutor should be nice'},
+                  { client_id: result[4], open: true, district_id: 5, location: 'Study Room', student_level_id: 5, tuition_fee: "$260/hr", num_of_student: 5, tutor_academic_id: 5, tutor_sex: 'M', times_per_week: '3 hrs', duration: '5 years', lesson_time: 'Friday 10:00am-1:00pm', hotline: '9000 0005', remark: 'the tutor should be kind'},
+                  { client_id: result[5], open: true, district_id: 6, location: 'at home', student_level_id: 6, tuition_fee: "$300/hr", num_of_student: 1, tutor_academic_id: 6, tutor_sex: 'O', times_per_week: '3.5 hrs', duration: '6 years', lesson_time: 'Saturday 10:00am-1:30pm', hotline: '9000 0006', remark: 'the tutor should be a nice person'},
+                  { client_id: result[6], open: true, district_id: 7, location: 'McCafe', student_level_id: 7, tuition_fee: "$400/hr", num_of_student: 2, tutor_academic_id: 7, tutor_sex: 'F', times_per_week: '4 hrs', duration: '1.5 years', lesson_time: 'Sunday 10:00am-2:00pm', hotline: '9000 0007', remark: 'the tutor should be a kind person'},
+                  { client_id: result[7], open: true, district_id: 8, location: 'SUBWAY', student_level_id: 8, tuition_fee: "$200/hr", num_of_student: 3, tutor_academic_id: 1, tutor_sex: 'M', times_per_week: '4.5 hrs', duration: '2.5 years', lesson_time: 'Sunday 10:00am-2:30pm', hotline: '9000 0008', remark: 'the tutor should be patient'},
+                  { client_id: result[8], open: true, district_id: 9, location: 'McCafe', student_level_id: 9, tuition_fee: "$265/hr", num_of_student: 4, tutor_academic_id: 2, tutor_sex: 'O', times_per_week: '5 hrs', duration: '3.5 years', lesson_time: 'Saturday, Sunday 10:00am-12:30pm', hotline: '9000 0009', remark: 'the tutor should be a patient guy'},
+                  { client_id: result[9], open: true, district_id: 10, location: 'SUBWAY', student_level_id: 10, tuition_fee: "$275/hr", num_of_student: 5, tutor_academic_id: 3, tutor_sex: 'F', times_per_week: '5.5 hrs', duration: '4.5 years', lesson_time: 'Monday, Tuesday 10:00am-12:45pm', hotline: '9000 0010', remark: 'the tutor should be nice'},
+                  { client_id: result[10], open: true, district_id: 11, location: 'Pacific Coffee', student_level_id: 11, tuition_fee: "$285/hr", num_of_student: 1, tutor_academic_id: 4, tutor_sex: 'M', times_per_week: '6 hrs', duration: '5.5 years', lesson_time: 'Tuesday, Wednesday 10:00am-1:00pm', hotline: '9000 0011', remark: 'the tutor should be a nice guy'},
+                  { client_id: result[11], open: true, district_id: 12, location: 'KFC', student_level_id: 12, tuition_fee: "$295/hr", num_of_student: 2, tutor_academic_id: 5, tutor_sex: 'O', times_per_week: '6.5 hrs', duration: 'until DSE', lesson_time: 'Wednesday, Thursday 10:00am-1:15pm', hotline: '9000 0012', remark: 'the tutor should be kind'},
+                  { client_id: result[12], open: true, district_id: 13, location: 'Study Room', student_level_id: 1, tuition_fee: "$215/hr", num_of_student: 3, tutor_academic_id: 6, tutor_sex: 'F', times_per_week: '7 hrs', duration: '1 year', lesson_time: 'Thursday, Friday 10:00am-1:30pm', hotline: '9000 0013', remark: 'the tutor should be patient'},
+                  { client_id: result[13], open: true, district_id: 14, location: 'Library', student_level_id: 2, tuition_fee: "$225/hr", num_of_student: 4, tutor_academic_id: 7, tutor_sex: 'M', times_per_week: '7.5 hrs', duration: 'until DSE', lesson_time: 'Friday, Saturday 10:00am-1:45pm', hotline: '9000 0014', remark: 'the tutor should be kind'},
+                  { client_id: result[14], open: true, district_id: 15, location: 'McDonald\'s', student_level_id: 3, tuition_fee: "$235/hr", num_of_student: 5, tutor_academic_id: 1, tutor_sex: 'O', times_per_week: '8 hrs', duration: '1 year', lesson_time: 'Monday, Wednesday, Friday, Saturday 10:30am-12:30pm', hotline: '9000 0015', remark: 'want a nice tutor'},
+                  { client_id: result[15], open: true, district_id: 16, location: 'Starbucks', student_level_id: 4, tuition_fee: "$245/hr", num_of_student: 1, tutor_academic_id: 2, tutor_sex: 'F', times_per_week: '6 hrs', duration: 'until graduation', lesson_time: 'Tuesday, Thursday, Saturday 9:00am-11:00am', hotline: '9000 0016', remark: 'want a kind tutor'},
+                  { client_id: result[0], open: true, district_id: 17, location: 'McDonald\'s', student_level_id: 5, tuition_fee: "$199/hr", num_of_student: 2, tutor_academic_id: 3, tutor_sex: 'M', times_per_week: '6 hrs', duration: '1 year', lesson_time: 'Wednesday, Friday, Sunday 1:00pm-3:00pm', hotline: '9000 0017', remark: 'want a nice tutor'},
+                  { client_id: result[0], open: true, district_id: 18, location: 'Starbucks', student_level_id: 6, tuition_fee: "$399/hr", num_of_student: 3, tutor_academic_id: 4, tutor_sex: 'O', times_per_week: '6 hrs', duration: 'until graduation', lesson_time: 'Friday, Saturday, Sunday 3:00pm-5:00pm', hotline: '9000 0018', remark: 'want a patient tutor'}
+              ])
+                  .returning("*")
+                  .then(function(result) {
+                      return knex('job_subject').insert([
+                          {job_id: result[0], subject_id: 1},
+                          {job_id: result[1], subject_id: 2},
+                          {job_id: result[2], subject_id: 3},
+                          {job_id: result[3], subject_id: 4},
+                          {job_id: result[4], subject_id: 5},
+                          {job_id: result[5], subject_id: 6},
+                          {job_id: result[6], subject_id: 7},
+                          {job_id: result[7], subject_id: 8},
+                          {job_id: result[8], subject_id: 9},
+                          {job_id: result[9], subject_id: 10},
+                          {job_id: result[10], subject_id: 11},
+                          {job_id: result[11], subject_id: 12},
+                          {job_id: result[12], subject_id: 13},
+                          {job_id: result[13], subject_id: 14},
+                          {job_id: result[14], subject_id: 15},
+                          {job_id: result[15], subject_id: 16},
+                          {job_id: result[16], subject_id: 17},
+                          {job_id: result[17], subject_id: 18}
+                      ])
+                  })
+          })
   ])
 };
