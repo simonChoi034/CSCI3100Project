6b5a04b5ac3de04c7c8d020ca370cffd4f74ca2a
SteinsFu <fuwingwa@hotmail.com>
2019-04-12 14:23:01 +0800

    collapsable sidebar in messenger

--- client/src/components/messenger/Messenger.jsx
+++ client/src/components/messenger/Messenger.jsx
@@ -5,7 +5,8 @@ import MessageList from '../message_list/MessageList';
 import './Messenger.css';
 import {authenticationService} from "../auth/authentication.service";
 import {Role} from "../helper";
-import { Fade } from 'reactstrap';
+import { Fade, Button } from 'reactstrap';
+import Sidebar from 'react-sidebar';
 
 
 export default class Messenger extends Component {
@@ -14,7 +15,13 @@ export default class Messenger extends Component {
 
         this.state = {
             currentChatRoomData: null,
+            sidebarOpen: true
         };
+
+        // collapsable sidebar
+        this.mql = window.matchMedia('(min-width: 480px');
+        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
 
         // connect socket
         this.socket = client.connect('http://localhost:8080/');
@@ -29,10 +36,29 @@ export default class Messenger extends Component {
         }));
     }
 
+    componentWillMount() {
+        this.mql.addListener(this.mediaQueryChanged);
+    }
+
+    componentWillUnmount() {
+        this.mql.removeListener(this.mediaQueryChanged);
+    }
+
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.state.currentUser !== prevState.currentUser) {
             this.socket.emit('setSocketID', this.state.currentUser.id);
         }
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({sidebarOpen: open});
+    }
+
+    mediaQueryChanged() {
+        this.setState({
+            sidebarDocked: this.mql.matches,
+            sidebarOpen: false
+        })
     }
 
     handleChatRoom(data){
@@ -44,22 +70,35 @@ export default class Messenger extends Component {
     render() {
         return (
             <div className="messenger">
-                <div className="scrollable sidebar">
-                    <ConversationList handleChatRoom={this.handleChatRoom} socket={this.socket} currentUser={this.state.currentUser}/>
-                </div>
-
-                {
-                    this.state.currentChatRoomData &&
-                    <div className="scrollable content">
-                        <Fade>
-                            <MessageList
-                                chatRoomData={this.state.currentChatRoomData}
-                                currentUser={this.state.currentUser}
-                                socket={this.socket}
-                            />
-                        </Fade>
+                <Sidebar
+                  sidebar={
+                    <div className="scrollable sidebar">
+                        <ConversationList handleChatRoom={this.handleChatRoom} socket={this.socket} currentUser={this.state.currentUser}/>
                     </div>
-                }
+                  }
+                  open={this.state.sidebarOpen}
+                  docked={this.state.sidebarDocked}
+                  onSetOpen={this.onSetSidebarOpen}
+                  className="collapse-sidebar"
+                >
+                    <Button onClick={() => this.onSetSidebarOpen(true)}>
+                      Open sidebar
+                    </Button>
+                    {
+                        this.state.currentChatRoomData &&
+                        <div className="scrollable content massage-list-container">
+                            <Fade>
+                                <MessageList
+                                    chatRoomData={this.state.currentChatRoomData}
+                                    currentUser={this.state.currentUser}
+                                    socket={this.socket}
+                                />
+                            </Fade>
+                        </div>
+                    }
+                </Sidebar>
+                
+                
             </div>
         );
     }
